bbd4343cd3d59269fc3a7a89283ae14eaaad1cf3
Daniel Willenson <dwillens@gmail.com>
2013-08-23 05:02:02 -0700

    completed disassembler

--- ARM/Disassembler.hs
+++ ARM/Disassembler.hs
@@ -1,2 +1,61 @@
-module ARM.Disassembler where
+module ARM.Disassembler (disassemble, disassembleI) where
+  import ARM.InstructionSet
+  import Data.Bits
+  import Data.Word
+  import Text.Printf
+
+  disassemble :: [Word32] -> [Instruction]
+  disassemble = map disassembleI
+
   disassembleI :: Word32 -> Instruction
+  disassembleI w =
+    case intField 25 0x7 of
+      0x0 -> 
+        if enumField 4 0x1 
+          then
+            if enumField 7 0x1
+              then
+                case (intField 20 0x1, intField 6 0x1, intField 5 0x1) of
+                  (l, 0, 1)  -> MEM (toEnum l) cc sg HALF rd rn dir mo
+                  (0, 1, st) -> MEM (toEnum $ 1 - st) cc sg DOUBLE rd rn dir mo
+                  (1, 1, h)  -> MEM LDR cc sg (toEnum $ 1 + h) rd rn dir mo
+              else DP dpOp cc dpS rd rn (SR rm sh rs)
+          else DP dpOp cc dpS rd rn (SI rm sh shAmt)
+      0x1 -> DP dpOp cc dpS rd rn (IM rotate imm8)
+      0x2 -> MEM memOp cc False sz rd rn dir (MEMI imm12)
+      0x3 -> 
+        if enumField 4 0x1
+          then undefined
+          else MEM memOp cc False sz rd rn dir (MEMR rm sh shAmt)
+      0x5 -> B cc lnk bOffset
+      otherwise -> undefined
+    where enumField :: (Enum a) => Int -> Word32 -> a
+          enumField = extractEnum w
+          intField :: (Integral a) => Int -> Word32 -> a
+          intField = extract w
+          cc = enumField 28 0xF
+          dpOp = enumField 21 0xF
+          memOp = enumField 20 0x1
+          dpS = enumField 20 0x1
+          rn = enumField 16 0xF
+          rd = enumField 12 0xF
+          rm = enumField 0 0xF
+          shAmt = intField 7 0x1F
+          sh = enumField 5 0x3
+          rotate = intField 8 0xF `shiftL` 1
+          imm8 = intField 0 0xFF
+          sz = enumField 22 0x1
+          dir = enumField 23 0x1
+          imm12 = intField 0 0xFFF
+          rs = enumField 8 0xF
+          mo = if enumField 22 0x1 then MEMI miscImm else MEMR rm LSL 0
+          sg = enumField 20 0x1 && enumField 6 0x1
+          miscImm = fromIntegral ((w .&. 0xF00) `shiftR` 4) .|. intField 0 0xF
+          lnk = enumField 24 0x1
+          bOffset = printf "%06x" $ (intField 0 0xFFFFFF :: Word32)
+
+  extractEnum :: (Enum a) => Word32 -> Int -> Word32 -> a
+  extractEnum w s m = toEnum $ extract w s m
+
+  extract :: (Integral a) => Word32 -> Int -> Word32 -> a
+  extract w s m = fromIntegral $ (.&. m) $ w `shiftR` s
--- Test_ARMAssembler.hs
+++ Test_ARMAssembler.hs
@@ -1,68 +1,88 @@
 module Main where
   import ARM.Assembler
+  import ARM.Disassembler
+  import ARM.InstructionSet as I
   import Text.Printf
   import Data.Word
   import Data.List
   import Test.HUnit
 
-  dp, mem :: [([Word32], String)]
-  dp =  [([0xE0910002], "DP ADD AL True R0 R1 (SI R2 LSL 0)")
-        ,([0x304948AB], "DP SUB CC False R4 R9 (SI R11 LSR 17)")
-        ,([0x11973275], "DP ORR NE True R3 R7 (SR R5 ROR R2)")
-        ,([0x73778926], "DP CMN VC True R8 R7 (IM 18 38)")
+  dp =  [([0xE0910002], [DP ADD AL True R0 R1 (SI R2 LSL 0)])
+        ,([0x304948AB], [DP SUB CC False R4 R9 (SI R11 LSR 17)])
+        ,([0x11973275], [DP ORR NE True R3 R7 (SR R5 ROR R2)])
+        ,([0x73778926], [DP CMN VC True R8 R7 (IM 18 38)])
         ]
 
-  mem = [([0xB51F2064], "MEM LDR LT False WORD R2 R15 DOWN (MEMI 100)")
-        ,([0x87C19FC3], "MEM STR HI False BYTE R9 R1 UP (MEMR R3 ASR 31)")
-        ,([0x015CDEFB], "MEM LDR EQ True HALF R13 R12 DOWN (MEMI 235)")
-        ,([0x011CD0F9], "MEM LDR EQ True HALF R13 R12 DOWN (MEMR R9 LSL 0)")
-        ,([0xA1C7AADE], "MEM LDR GE False DOUBLE R10 R7 UP (MEMI 174)")
-        ,([0xA187A0D2], "MEM LDR GE False DOUBLE R10 R7 UP (MEMR R2 LSL 0)")
-        ,([0x41C681F6], "MEM STR MI False DOUBLE R8 R6 UP (MEMI 22)")
-        ,([0x418680FB], "MEM STR MI False DOUBLE R8 R6 UP (MEMR R11 LSL 0)")
-        ,([0x515C40B7], "MEM LDR PL False HALF R4 R12 DOWN (MEMI 7)")
-        ,([0x511C40B8], "MEM LDR PL False HALF R4 R12 DOWN (MEMR R8 LSL 0)")
-        ,([0x615A52DA], "MEM LDR VS True BYTE R5 R10 DOWN (MEMI 42)")
-        ,([0x611A50DF], "MEM LDR VS True BYTE R5 R10 DOWN (MEMR R15 LSL 0)")
-        ,([0xC1CE3EBB], "MEM STR GT False HALF R3 R14 UP (MEMI 235)")
-        ,([0xC18E30B9], "MEM STR GT False HALF R3 R14 UP (MEMR R9 LSL 0)")
+  mem = [([0xB51F2064], [MEM LDR I.LT False WORD R2 R15 DOWN (MEMI 100)])
+        ,([0x87C19FC3], [MEM STR HI False BYTE R9 R1 UP (MEMR R3 ASR 31)])
+        ,([0x015CDEFB], [MEM LDR I.EQ True HALF R13 R12 DOWN (MEMI 235)])
+        ,([0x011CD0F9], [MEM LDR I.EQ True HALF R13 R12 DOWN (MEMR R9 LSL 0)])
+        ,([0xA1C7AADE], [MEM LDR GE False DOUBLE R10 R7 UP (MEMI 174)])
+        ,([0xA187A0D2], [MEM LDR GE False DOUBLE R10 R7 UP (MEMR R2 LSL 0)])
+        ,([0x41C681F6], [MEM STR MI False DOUBLE R8 R6 UP (MEMI 22)])
+        ,([0x418680FB], [MEM STR MI False DOUBLE R8 R6 UP (MEMR R11 LSL 0)])
+        ,([0x515C40B7], [MEM LDR PL False HALF R4 R12 DOWN (MEMI 7)])
+        ,([0x511C40B8], [MEM LDR PL False HALF R4 R12 DOWN (MEMR R8 LSL 0)])
+        ,([0x615A52DA], [MEM LDR VS True BYTE R5 R10 DOWN (MEMI 42)])
+        ,([0x611A50DF], [MEM LDR VS True BYTE R5 R10 DOWN (MEMR R15 LSL 0)])
+        ,([0xC1CE3EBB], [MEM STR I.GT False HALF R3 R14 UP (MEMI 235)])
+        ,([0xC18E30B9], [MEM STR I.GT False HALF R3 R14 UP (MEMR R9 LSL 0)])
         ]
 
   branch = [([0xEA000000, 0x00000000], 
-             "B AL False \"Test\"\n" ++
-             "Label \"Test\" (DP AND EQ False R0 R0 (SI R0 LSL 0))")
-           ,([0xEAFFFFFC], "Label \"Test\" (B AL False \"Test\")")
-           ,([0x00000000
-             ,0xEAFFFFF8], 
-             "Label \"A\" (DP AND EQ False R0 R0 (SI R0 LSL 0))\n" ++
-             "B AL False \"A\"")
+             [B AL False "000000"
+             ,I.Label "000000" (DP AND I.EQ False R0 R0 (SI R0 LSL 0))])
+           ,([0xEAFFFFFC], [I.Label "fffffc" (B AL False "fffffc")])
+           ,([0x00000000, 0xEAFFFFF8],
+             [I.Label "fffff8" (DP AND I.EQ False R0 R0 (SI R0 LSL 0))
+             ,B AL False "fffff8"])
            ,([0xEB000000, 0x00000000], 
-             "B AL True \"Test\"\n" ++ 
-             "Label \"Test\" (DP AND EQ False R0 R0 (SI R0 LSL 0))")
-           ,([0xEBFFFFFC], "Label \"Test\" (B AL True \"Test\")")
+             [B AL True "000000" 
+             ,I.Label "000000" (DP AND I.EQ False R0 R0 (SI R0 LSL 0))])
+           ,([0xEBFFFFFC], [I.Label "fffffc" (B AL True "fffffc")])
            ,([0x00000000, 0xEBFFFFF8], 
-             "Label \"A\" (DP AND EQ False R0 R0 (SI R0 LSL 0))\n" ++ 
-             "B AL True \"A\"")
+             [I.Label "fffff8" (DP AND I.EQ False R0 R0 (SI R0 LSL 0)) 
+             ,B AL True "fffff8"])
            ]
 
-  str = [([0x44434241], "DataString \"ABCD\"")
-        ,([0x00004241], "DataString \"AB\"")
-        ,([0x44434241, 0x48474645], "DataString \"ABCDEFGH\"")
+  str = [([0x44434241], [DataString "ABCD"])
+        ,([0x00004241], [DataString "AB"])
+        ,([0x44434241, 0x48474645], [DataString "ABCDEFGH"])
         ,([0xE28FCF01, 0x44434241, 0x48474645], 
-          "LoadAddress R12 \"world\"\n" ++ 
-          "Label \"hello\" (DataString \"ABCD\")\n" ++ 
-          "Label \"world\" (DataString \"EFGH\")")
+          [LoadAddress R12 "world" 
+          ,I.Label "hello" (DataString "ABCD")
+          ,I.Label "world" (DataString "EFGH")])
         ,([0xE28FCF03, 0x41414141, 0x41414141, 0x41414141, 0x48474645], 
-          "LoadAddress R12 \"world\"\n" ++ 
-          "Label \"hello\" (DataString \"" ++ 
-            take 12 (repeat 'A') ++ "\")\n" ++ 
-          "Label \"world\" (DataString \"EFGH\")")
+          [LoadAddress R12 "world" 
+          ,I.Label "hello" (DataString "AAAAAAAAAAAA")
+          ,I.Label "world" (DataString "EFGH")])
         ]
 
-  main = runTestTT $ TestList $ map test (dp ++ mem ++ branch ++ str)
-    where test :: ([Word32], String) -> Test
-          test (is, s) = TestCase $ 
-            assertEqual ("test [" ++
-                         intercalate "," (map (printf "0x%08x") is) ++
-                         "] <= " ++ s) is $
-              assemble $ map read $ lines s
+  main = runTestTT $ TestList $ [testAssembler, testDisassembler]
+
+
+  testAssembler :: Test
+  testAssembler = TestList $ map (uncurry test) (dp ++ mem ++ branch ++ str)
+    where test :: [Word32] -> [Instruction] -> Test
+          test ws is = TestCase $ assertEqual (name ws is) ws $ assemble is
+          name :: [Word32] -> [Instruction] -> String
+          name ws is = "assemble [" ++ commaList (map show is) ++ "] => " ++
+                       "[" ++ commaList (map (printf "0x%08x") ws) ++ "]"
+
+  testDisassembler :: Test
+  testDisassembler = TestList $ map (uncurry test) (dp ++ mem ++ branch')
+    where test :: [Word32] -> [Instruction] -> Test
+          test ws is = TestCase $ assertEqual (name ws is) is $ disassemble ws
+          name :: [Word32] -> [Instruction] -> String
+          name ws is = "disassemble [" ++ commaList (map hex ws) ++ "] => " ++
+                       "[" ++ commaList (map show is) ++ "]"
+          branch' = map (\(ws, is) -> (ws, map removeLabels is)) branch
+          removeLabels :: Instruction -> Instruction
+          removeLabels (I.Label _ i) = i
+          removeLabels i = i
+
+  commaList :: [String] -> String
+  commaList = intercalate ", "
+
+  hex :: Word32 -> String
+  hex = printf "0x%08x"
